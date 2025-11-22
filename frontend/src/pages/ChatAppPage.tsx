1010f271f851af18cb165ecd427d2a18d3eef2c3
Tran Hai Son <sonthse172913@fpt.edu.vn>
2025-11-22 14:29:33 +0700

    fix(auth): ensure refresh token keeps session alive

--- frontend/src/pages/ChatAppPage.tsx
+++ frontend/src/pages/ChatAppPage.tsx
@@ -1,10 +1,16 @@
-import Logout from '@/components/auth/logout'
-import React from 'react'
+import Logout from '@/components/auth/Logout'
+import { useAuthStore } from '@/stores/useAuthStore';
 
 const ChatAppPage = () => {
+  const user =  useAuthStore((state) => state.user);
+
+
+
   return (
     <div>
+      {user?.username}
       <Logout/>
+
     </div>
   )
 }
--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -22,7 +22,15 @@ export const authService = {
 
     signOut: async () => {
         return api.post('/auth/signout', {}, { withCredentials: true });
-    }
+    },
+    
+    fetchMe: async () => {
+        const res = await api.get('/users/me', { withCredentials: true });
+        return res.data.user;
+    },
 
-    fetchMe
+    refresh: async () =>{
+        const res = await api.post("/auth/refresh", {withCredentials: true});
+        return res.data.accessToken;
+    }
 };
